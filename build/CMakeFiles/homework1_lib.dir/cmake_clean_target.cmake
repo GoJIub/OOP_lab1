8db896e5cdac835191d5a38cb54f4e709e5672ab
Zhenia <zhenia.saharow@yandex.ru>
2025-09-24 17:41:46 +0300

    Add .gitignore

--- build/CMakeFiles/homework1_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libhomework1_lib.a"
-)
--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Projects/OOP/Homeworks/1
-# Build directory: D:/Projects/OOP/Homeworks/1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(MyProjectTests "D:/Projects/OOP/Homeworks/1/build/tests.exe")
-set_tests_properties(MyProjectTests PROPERTIES  _BACKTRACE_TRIPLES "D:/Projects/OOP/Homeworks/1/CMakeLists.txt;36;add_test;D:/Projects/OOP/Homeworks/1/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
